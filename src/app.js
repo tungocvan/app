f6e0f79372d8db3d4f200d91a4a48a99c892a2b9
tungocvan <tungocvan@gmail.com>
2020-12-08 13:56:49 +0700

    v2

--- src/app.js
+++ src/app.js
@@ -4,8 +4,8 @@ const handlebars = require('express-handlebars');
 const route = require('./routes');
 const app = express();
 const port = process.env.PORT || 3000;
-//const db = require('./config/db/');
-//db.connect();
+const db = require('./config/db/');
+db.connect();
 // Use static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
