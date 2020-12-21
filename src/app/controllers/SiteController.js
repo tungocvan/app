481ebf6af0360d49445cfd77175b39a4abc0a460
tungocvan <tungocvan@gmail.com>
2020-12-21 22:08:23 +0700

    relx4

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,17 +1,28 @@
-const modelUser = require('../models/User');
-const {mutileMongooseToObject , mongooseToObject} = require('../models/mongoose');
+//const modelUser = require('../models/User');
+//const {mutileMongooseToObject , mongooseToObject} = require('../models/mongoose');
+const products = [
+                    {"id":1,"title":"Sạc RELX Kits chỉ còn 599k !!","price":"599.000"},
+                    {"id":3,"title":"RELX Infinity Pods.","price":"150.000"},
+                    {"id":5,"title":"Thiết bị Pod RELX Nano","price":"150.000"},
+                    {"id":7,"title":"Hệ thống Relx Pod có sẵn 1 pod đầu tiên cho bạc hà và xì gà với quà tặng miễn phí","price":"850.000"},
+                    {"id":2,"title":"Thiết bị cần thiết RELX !!","price":"700.000"},
+                    {"id":4,"title":"RELX Infinity (RELX thế hệ thứ 4).","price":"900.000"},
+                    {"id":6,"title":"Relx Pod - Pack 3 Pod Dầu Cho Relx Pod System Chính hãng","price":"350.000"}
+                 ];
 class SiteController {
   // [GET] /
   index(req, res) {
     res.render('home',{layout : 'relx'});
   } 
   postDetail(req, res) {
-    res.render('postDetail',{layout : 'relx'});
+    //console.log('id:',req.params.id);
+    let item = products.find(value => value.id == req.params.id);
+    res.render('postDetail',{layout : 'relx' , item});
   } 
 
   // [GET] /search
-  search(req, res) {
-    res.render('search');
+  sanpham(req, res) {
+    res.render('sanpham',{layout : 'relx'});
   }
 }
 
--- src/routes/site.js
+++ src/routes/site.js
@@ -3,8 +3,9 @@ const router = express.Router();
 
 const siteController = require('../app/controllers/SiteController');
 
-router.use('/search', siteController.search);
-router.use('/postDetail/:id', siteController.postDetail);
-router.use('/', siteController.index);
+//router.use('/search', siteController.search);
+router.get('/postDetail/:id', siteController.postDetail);
+router.get('/san-pham', siteController.sanpham);
+router.get('/', siteController.index);
 
 module.exports = router;
